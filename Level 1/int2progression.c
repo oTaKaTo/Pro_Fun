269faa25850d69a5c84905dece0055f503269c32
Takdanai <jetak55.tak@gmail.com>
2022-08-02 21:53:43 +0700

    Level 1

---
+++ Level 1/int2progression.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+    int i,j ;
+    float k = i/j ;
+    scanf("%d %d",&i,&j) ;
+    
+    printf("The result of addiction = %d\n",i+j) ;
+    printf("The result of subtraction = %d\n",i-j) ;
+    printf("The result of multiplication = %d\n", i*j) ;
+    printf("The result of division = %f\n",k) ;
+    return 0 ;
+
+}
