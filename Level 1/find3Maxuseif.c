bd27bd1fddbdb0c3035a4c5670c485ff62fb1e9d
Takdanai <jetak55.tak@gmail.com>
2022-08-03 15:15:52 +0700

    Level 1

---
+++ Level 1/find3Maxuseif.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () 
+{
+    int x , y ,z ;
+    printf("Enter 3 numbers : ");
+    scanf("%d %d %d",&x,&y,&z);
+
+    if(x>y && x>z)
+    {
+        printf("The highest number : %d",x) ;
+    }
+    else if(y>x && y>z)
+    {
+        printf("The highest number : %d",y) ;
+    }
+    else if(z>x && z>y)
+    {
+        printf("The highest number : %d",z) ;
+    }
+    else
+    {
+        printf("There's not have highest number");
+    }
+    return 0;
+}
