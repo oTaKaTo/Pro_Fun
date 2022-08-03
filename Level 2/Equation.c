e36ea8c98939460688a621277c97dda87bac8967
Takdanai <jetak55.tak@gmail.com>
2022-08-03 14:54:15 +0700

    Level 2

--- Level 2/Equation.c
+++ Level 2/Equation.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
 #include <math.h>
 
+int powerrr(x,y)
+{
+    int value ;
+    for(int n=1 ; n == y ; n++)
+    {
+        x = x*y ;
+    }
+    return x ;
+}
+
+
 int main ()
 {
-    int a ,b , c;
+    int a ,b , c , number;
+    int x ,y;
+    x = a ;
+    y = b;
     
+    printf("Enter 2 number : ");
     scanf("%d %d",&a,&b) ;
     
+    printf("1. c =  2*a + b\n2. c = a^5 + a^4 + 2a^3 + 3a^2 + 4a + 3\n3. c = 2 + 3a\nChoose 1 of 3 equations : ") ;
+    scanf("%d",&number);
     
-    //c =  2*a + b ;
-    c = pow(a,5) + pow(a,4) + 2*pow(a,3) + 3*pow(a,2) + 4*a + 3 ;
-    //c = 2 + (3*a) ;
+    switch (number)
+    {
+    case 1:
+        c =  2*a + b ;
+        break;
+    case 2:
+        c = powerrr(a,5) + powerrr(a,4) + 2*powerrr(a,3) + 3*powerrr(a,2) + 4*a + 3 ; 
+        break;
+        //c = round(pow(a,5)) + round(pow(a,4)) + 2*round(pow(a,3)) + 3*round(pow(a,2)) + 4*a + 3 ;
+    case 3:
+        c = 2 + (3*a) ;
+    default:
+        break;
+    }
+
 
     printf("%d",c) ;
 
