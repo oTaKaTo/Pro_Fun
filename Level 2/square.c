51c2547b7aea5af79c141f48c18423f62895b0de
Takdanai <jetak55.tak@gmail.com>
2022-08-03 16:11:10 +0700

    Level 2

---
+++ Level 2/square.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int size ;
+
+    scanf ("%d",&size);
+
+    for(int i = 0 ; i<size ; i++)
+    {
+        for(int j=0 ; j<size ; j++)
+        {
+            printf("*");
+        }
+        printf("\n") ;
+    }
+    return 0;
+}
