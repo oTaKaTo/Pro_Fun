e97779becd2c84fab67593004ea7a719cd8efba3
Takdanai <jetak55.tak@gmail.com>
2022-08-03 16:49:18 +0700

    Level 2

---
+++ Level 2/Chess_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int size ;
+    
+    scanf("%d",&size);
+
+    for(int i = 0 ; i<size ; i++)
+    {
+        for(int j = 0 ; j<size ; j++)
+        {
+            int k = i+j;
+            if(k % 2 == 1)
+            printf(" ") ;                 
+            else
+            printf("*");
+        }
+        printf("\n") ;
+    }
+
+
+    return 0;
+}
